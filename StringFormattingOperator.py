9a8bbef0eb0938157422aac21a4429835481d06c
Yussef raouf <yussef.raouf11@gmail.com>
2022-10-14 18:29:53 +0200

    strings part 2

---
+++ StringFormattingOperator.py
@@ -0,0 +1,7 @@
+str = "yussef is %d years old"%(12) #decimal
+str = "yussef is %f years old"%(12) #float
+str = "yussef is %0.2f years old"%(12) #float with two integers after 0.
+str = "yussef is %s years old"%(12) #string
+
+
+
---
+++ StringIndicies&Slicing.py
@@ -0,0 +1,5 @@
+str = 'yussef'
+str[0]#y
+str[-1]#f
+str[1:3] #us
+str[::-1] #reverse
